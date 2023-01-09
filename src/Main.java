29faf057215ab180f62fcc066a4d9ad510f3893e
Saksham Singh Khanuja <sskhanuja94@gmail.com>
2023-01-09 16:21:15 +0530

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 34, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+        fizzBuzz(numbers);
+    }
+
+    /**
+     * Replaces elements of array with "Fizz", "Buzz" or "FizzBuzz" based on index values.
+     */
+    public static void fizzBuzz(int[] numbers) {
+        for (int i = 0; i < numbers.length; i++) {
+            if ((numbers[i] % 3 == 0) && (numbers[i] % 5 == 0)) {
+                System.out.print("FizzBuzz ");
+            } else if (numbers[i] % 3 == 0) {
+                System.out.print("Fizz ");
+            } else if (numbers[i] % 5 == 0) {
+                System.out.print("Buzz ");
+            } else {
+                System.out.print(numbers[i] + " ");
+            }
+        }
+    }
+}
